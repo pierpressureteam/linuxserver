e69d7f8a839c5c45451d3a2d025c982d9418723a
oveldman <oveldman@gmail.com>
2016-05-19 13:16:15 +0200

    Fix crash by server down

--- src/com/pier/ApiToken.java
+++ src/com/pier/ApiToken.java
@@ -14,11 +14,20 @@ public class ApiToken {
 
     public void setToken() throws Exception{
             JSONParser tokenParser = new JSONParser();
+        try {
+
+
             Object tokenObject = tokenParser.parse(new FileReader("../JSON/apiLogin.json"));
+
             JSONObject tokenJsonObject = (JSONObject) tokenObject;
+
             String tokenReal = (String) tokenJsonObject.get("token");
             System.out.println(tokenReal);
             token = tokenReal;
+        }catch(Exception e){
+            System.out.println("Server is down");
+            System.exit(1);
+        }
         }
     }
 
